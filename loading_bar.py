3d626cb9e8f0622123696a1248f8e233c992b933
ddoberne <ddoberne@gmail.com>
2022-11-02 13:00:15 -0700

    Create loading_bar.py

---
+++ loading_bar.py
@@ -0,0 +1,28 @@
+ticks = {0:'|',
+           1:'/',
+           2:'—',
+           3:'\\'}
+
+def loading_str(i:'int', t:'int', verbose = False, text = 'iteration') -> 'str':
+    '''Returns a string with a loading bar for iteration i out of t.'''
+    progress = (10 * i)//t
+    tick = ticks[i%4]
+    to_go = 10 - progress
+    output = f'[{progress * "*"}{tick}{to_go * "."}]'
+    if verbose: output += f' {text} {i}/{t}'
+    return output
+    
+def loading(i:'int', t:'int', verbose = False, text = 'iteration') -> 'str':
+    '''Prints a loading bar for iteration i out of t'''
+    print(loading_str(i,t,verbose,text), end = '\r')
+    
+class LoadingBar:
+    def __init__(self, t, verbose = False, text = 'iteration'):
+        self.t = t
+        self.i = 0
+        self.verbose = verbose
+        self.text = text
+        
+    def display(self):
+        self.i += 1
+        loading(self.i, self.t, self.verbose, self.text)
